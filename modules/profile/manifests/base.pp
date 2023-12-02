8acd549f2e03301ce71d00a274bb168213146ecd
ArthurWuTW <lion811004@gmail.com>
2023-12-02 09:19:50 -0500

    installing kubelet, kubeadm, kubectl

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -1,6 +1,8 @@
 class profile::base {
   require profile::docker_install
 
+  $kube_packages = ['kubelet', 'kubeadm', 'kubectl']
+
   service { 'Service Docker...':
     ensure => running,
     name   => 'docker',
@@ -12,4 +14,15 @@ class profile::base {
     name   => 'firewalld',
     enable => false
   }
+
+  file { '/etc/yum.repos.d/kubernetes.repo':
+    mode  => '0744',
+    owner => root,
+    group => root,
+    source => "puppet:///modules/profile/kubernetes.repo"
+  } -> package { $kube_packages:
+    ensure => installed,
+    require => File['/etc/yum.repos.d/kubernetes.repo']
+  }
+
 }
